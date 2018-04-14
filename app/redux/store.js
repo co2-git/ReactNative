fd27c31c56fa8e1b3aa7edc32710ec61067a4e00
co2-git <francoisrvespa@gmail.com>
2018-04-14 14:07:47 +0100

    Store

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,9 +1,5 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  createStore,
-} from 'redux';
-import {persistStore, persistReducer} from 'redux-persist';
+import {combineReducers, createStore} from 'redux';
+import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import * as reducers from './reducers';
@@ -14,7 +10,5 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, combineReducers(reducers));
-
-// export default createStore(combineReducers(reducers));
 
 export default createStore(persistedReducer);
