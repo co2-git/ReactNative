26e4655cad2414a69f51818df18b8dbbe8288f3a
co2-git <francoisrvespa@gmail.com>
2018-04-15 00:13:18 +0100

    Fix #3

--- app/index.js
+++ app/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import fixPath from 'fix-path';
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import {app, autoUpdater} from 'electron';
+import isDev from 'electron-is-dev';
 
 import App from './App';
+import config from '../config.json';
 
 fixPath();
 
 const root = document.getElementById('root');
 
 ReactDOM.render(<App />, root);
+
+if (!isDev) {
+  const server = config.now.url;
+  const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+  autoUpdater.setFeedURL(feed);
+
+  setInterval(() => {
+    autoUpdater.checkForUpdates();
+  }, 1000 * 60 * 60 * 24);
+}
