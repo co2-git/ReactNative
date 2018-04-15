547ac9a190619a9c06efed376eb5bc2c3011e24c
co2-git <francoisrvespa@gmail.com>
2018-04-15 14:29:59 +0100

    Fix #24

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,5 +1,6 @@
 // @flow
 export {default as androidRunStatus} from './reducers/androidRunStatusReducer';
+export {default as appRouterIndex} from './reducers/appRouterIndexReducer';
 export {default as apps} from './reducers/appsReducer';
 export {default as error} from './reducers/errorReducer';
 export {default as info} from './reducers/infoReducer';
